507458e5fe14691c3cdf509c4a9969e66e9a6e14
notryancho <ryancho1226@hotmail.com>
2023-03-29 02:02:38 -0400

    user resource update

---
+++ resources/user.py
@@ -0,0 +1,51 @@
+from flask_restful import Resource, reqparse
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from bson.objectid import ObjectId
+from app import User
+
+
+class UserResource(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('email', type=str, required=True, help='Email is required.')
+    parser.add_argument('password', type=str, required=True, help='Password is required.')
+
+    @jwt_required()
+    def get(self, user_id):
+        user = User.objects(id=ObjectId(user_id)).first()
+        if not user:
+            return {'message': 'User not found.'}, 404
+        return {'email': user.email, 'is_customer': user.is_customer, 'is_nail_tech': user.is_nail_tech}, 200
+
+    def post(self):
+        data = UserResource.parser.parse_args()
+        email = data['email']
+        password = data['password']
+
+        existing_user = User.objects(email=email).first()
+        if existing_user:
+            return {'message': 'User with email already exists.'}, 409
+
+        user = User(email=email, password=password)
+        user.save()
+        return {'message': 'User created successfully.'}, 201
+
+    @jwt_required()
+    def put(self, user_id):
+        user = User.objects(id=ObjectId(user_id)).first()
+        if not user:
+            return {'message': 'User not found.'}, 404
+
+        data = UserResource.parser.parse_args()
+        user.email = data.get('email', user.email)
+        user.password = data.get('password', user.password)
+        user.save()
+        return {'message': 'User updated successfully.'}, 200
+
+    @jwt_required()
+    def delete(self, user_id):
+        user = User.objects(id=ObjectId(user_id)).first()
+        if not user:
+            return {'message': 'User not found.'}, 404
+
+        user.delete()
+        return {'message': 'User deleted successfully.'}, 200
