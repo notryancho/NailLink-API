e598c65b795187de30baefb69e3f4010eda7185f
notryancho <ryancho1226@hotmail.com>
2023-03-28 23:50:22 -0400

    customer resource update

--- resources/customer.py
+++ resources/customer.py
@@ -1,7 +1,8 @@
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask import jsonify, request
 from flask_restful import Resource
 from mongoengine.errors import DoesNotExist, ValidationError
-from models import Customer as CustomerModel
+from app. import Customer as CustomerModel
 
 class Customer(Resource):
     def get(self, customer_id=None):
