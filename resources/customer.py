2f2052545ec096398f9dd1d48ce4d57e836b78a8
notryancho <ryancho1226@hotmail.com>
2023-03-28 23:50:00 -0400

    customer resource

---
+++ resources/customer.py
@@ -0,0 +1,42 @@
+from flask import jsonify, request
+from flask_restful import Resource
+from mongoengine.errors import DoesNotExist, ValidationError
+from models import Customer as CustomerModel
+
+class Customer(Resource):
+    def get(self, customer_id=None):
+        if customer_id:
+            try:
+                customer = CustomerModel.objects.get(id=customer_id)
+            except (DoesNotExist, ValidationError):
+                return {"message": "Customer not found"}, 404
+            return jsonify(customer)
+        else:
+            customers = CustomerModel.objects.all()
+            return jsonify(customers)
+
+    def post(self):
+        body = request.get_json()
+        customer = CustomerModel(**body)
+        customer.save()
+        return jsonify(customer)
+
+    def put(self, customer_id):
+        try:
+            customer = CustomerModel.objects.get(id=customer_id)
+        except (DoesNotExist, ValidationError):
+            return {"message": "Customer not found"}, 404
+
+        body = request.get_json()
+        customer.update(**body)
+        customer.reload()
+        return jsonify(customer)
+
+    def delete(self, customer_id):
+        try:
+            customer = CustomerModel.objects.get(id=customer_id)
+        except (DoesNotExist, ValidationError):
+            return {"message": "Customer not found"}, 404
+
+        customer.delete()
+        return {"message": "Customer deleted"}
